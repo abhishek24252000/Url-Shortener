ebb9080f719dabd9d0c5948906f29451b127bd38
abhishek24252000 <aringaleabhishek@gmail.com>
2025-04-10 14:25:41 +0530

    feat: add authentication and user context with login functionality
    - Implemented login functionality using Supabase in apiAUth.js
    - Created UrlProvider for user state management in context.jsx
    - Added Error component for error handling in Login component
    - Integrated Yup for form validation in Login component
    - Updated App component to include UrlProvider
    - Added react-spinners for loading indication during login
    - Introduced Card components for better UI structure in Login
    - Updated package.json and package-lock.json to include new dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import AppLayout from './layout/app-layout'
-import LandingPage from './pages/LandingPage'
-import Auth from './pages/Auth'
-import Link from './pages/Link'
-import RedirectLink from './pages/RedirectLink'
-import Dashboard from './pages/Dashboard'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import AppLayout from './layout/app-layout';
+import LandingPage from './pages/LandingPage';
+import Auth from './pages/Auth';
+import Link from './pages/Link';
+import RedirectLink from './pages/RedirectLink';
+import Dashboard from './pages/Dashboard';
+import UrlProvider from './context';
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-        path:"/",
-        element:<LandingPage/>
+        path: '/',
+        element: <LandingPage />,
       },
       {
-        path:"/dashboard",
-        element:<Dashboard/>
+        path: '/dashboard',
+        element: <Dashboard />,
       },
       {
-        path:"/auth",
-        element:<Auth/>
+        path: '/auth',
+        element: <Auth />,
       },
       {
-        path:"/link/:id",
-        element:<Link/>
+        path: '/link/:id',
+        element: <Link />,
       },
       {
-        path:"/:id",
-        element:<RedirectLink/>
-      }
-    ]
-  }
-])
+        path: '/:id',
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
   return (
-   <RouterProvider router={router}/>
-  )
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
 }
 
-export default App
+export default App;
